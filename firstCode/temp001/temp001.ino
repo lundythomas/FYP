c930647f08046de93df780c55dd6725aa4475fc2
Tom Lundy <lundythomas@hotmail.com>
2017-11-02 11:08:32 +0000

    02112017 commit

---
+++ firstCode/temp001/temp001.ino
@@ -0,0 +1,28 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN            7         // Pin which is connected to the DHT sensor.
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+void setup() {
+  SerialUSB.begin(9600);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  delayMS = sensor.min_delay / 1000;
+}
+
+void loop() {
+  delay(delayMS);  // Delay between measurements.
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    SerialUSB.println("Error reading temperature!");
+  }
+  else {
+    SerialUSB.println(event.temperature);
+  }
+}
